769714c2325b781ae47f77d3647072043f9da7f3
ajharul abedeen <ajharulabedeen@gmail.com>
2019-12-21 19:05:02 +0600

    Database configuration class added from Working JPA code.

--- back_end/org.gobeshona/src/main/java/org/gobeshona/configs/StatusRequestSingleton.java
+++ back_end/org.gobeshona/src/main/java/org/gobeshona/configs/StatusRequestSingleton.java
@@ -1,17 +1,17 @@
-package org.gobeshona.configs;
-
-import org.gobeshona.pojos.StatusRequest;
-
-public class StatusRequestSingleton
-    {
-        private static StatusRequest statusRequest_obj;
-        
-        public static StatusRequest getStatusRequest()
-            {
-                if(statusRequest_obj == null)
-                  {
-                    statusRequest_obj = new StatusRequest();
-                  }
-                return statusRequest_obj;
-            }
-    }
+//package org.gobeshona.configs;
+//
+//import org.gobeshona.pojos.StatusRequest;
+//
+//public class StatusRequestSingleton
+//    {
+//        private static StatusRequest statusRequest_obj;
+//        
+//        public static StatusRequest getStatusRequest()
+//            {
+//                if(statusRequest_obj == null)
+//                  {
+//                    statusRequest_obj = new StatusRequest();
+//                  }
+//                return statusRequest_obj;
+//            }
+//    }
